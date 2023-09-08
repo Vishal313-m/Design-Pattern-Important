53744ab7ffc1375b3df5bbf7fbb6c7d975f3af81
Vishal Mahto <85690685+Vishal313-m@users.noreply.github.com>
2023-09-08 16:45:25 +0530

    Update Singleton.cpp

--- Design Pattern/Singleton.cpp
+++ Design Pattern/Singleton.cpp
@@ -1,2 +1,283 @@
-# Youtube Link --- https://www.youtube.com/watch?v=EZDeEHXUf8w&t=587s
+1- # Youtube Link --- https://www.youtube.com/watch?v=EZDeEHXUf8w&t=587s
 
+2- #Youtube Link   --   https://www.youtube.com/watch?v=d2nxqGAS9xo&list=PLk6CEY9XxSIDZhQURp6d8Sgp-A0yKKDKV&index=4
+
+3- #Youtube Link --   https://www.youtube.com/watch?v=IJKu2pebSwc&list=PLk6CEY9XxSIDZhQURp6d8Sgp-A0yKKDKV&index=4     <-- Thread Safety
+
+4- GFG -- https://www.geeksforgeeks.org/implementation-of-singleton-class-in-cpp/
+
+Sure thing! Let's break down the Singleton design pattern in a simple way for an 18-year-old.
+
+**Singleton Design Pattern:**
+
+**What is it?**
+Think of the Singleton pattern as a special guard that ensures there's only one of something in your computer program. 
+Imagine you have a super-secret diary, and you want to make sure there's only one copy of it, no matter how many people try to make a copy.
+  The Singleton pattern is like a magic spell that keeps your diary unique and safe.
+
+**Why do we need it?**
+Sometimes in computer programs, we want to make sure there's only one instance (a single object) of a particular thing. 
+This could be something like a game manager, a printer manager, or a database connection.
+We use the Singleton pattern to guarantee that there's only one of these things, which can help prevent problems and save memory.
+
+**How can we implement it in C++?**
+Here's a simple example in C++:
+
+```cpp
+#include <iostream>
+
+class Diary {
+private:
+    // This is where we keep the single instance of the diary
+    static Diary* instance;
+
+    // We make the constructor private so no one else can create a diary
+    Diary() {}
+
+public:
+    // This is how we get the one and only diary
+    static Diary* getInstance() {
+        if (instance == nullptr) {
+            instance = new Diary();
+        }
+        return instance;
+    }
+
+    // Here, you can add methods to write and read your diary
+    void writeEntry(const std::string& entry) {
+        // Write your diary entry code here
+        std::cout << "Wrote in my diary: " << entry << std::endl;
+    }
+};
+
+// Initialize the instance as nullptr
+Diary* Diary::instance = nullptr;
+
+int main() {
+    // Let's write an entry in our diary
+    Diary* myDiary = Diary::getInstance();
+    myDiary->writeEntry("Today was a great day!");
+
+    // Now, let's try to create another diary
+    Diary* anotherDiary = Diary::getInstance();
+    anotherDiary->writeEntry("This is the second diary entry.");
+
+    return 0;
+}
+```
+
+**Pros of Singleton:**
+
+  1. Guarantees there's only one instance of something, which can be helpful in various situations.
+
+  2. Saves memory because it creates the instance only when needed.
+
+  3. Provides a convenient way to access that single instance.
+
+4.  You can be sure that a class has only a single instance.
+
+5.You gain a global access point to that instance.
+
+6. The singleton object is initialized only when it’s requested for the first time.
+
+**Cons of Singleton:**
+
+  1. Can make the code more complex.
+
+  2. Can introduce global state, making it harder to track and debug issues.
+
+  3. Needs careful handling in multi-threaded programs to avoid problems.
+
+4. Violates the Single Responsibility Principle. The pattern solves two problems at the time.
+
+  5.  The Singleton pattern can mask bad design, for instance, when the components of the program know too much about each other.
+
+  6. The pattern requires special treatment in a multithreaded environment so that multiple threads won’t create a singleton object several times.
+
+  7. It may be difficult to unit test the client code of the Singleton because many test frameworks rely on inheritance when producing mock objects. 
+  Since the constructor of the singleton class is private and overriding static methods is impossible in most languages, 
+  you will need to think of a creative way to mock the singleton. Or just don’t write the tests. Or don’t use the Singleton pattern.
+
+So, the Singleton pattern is like a guardian that ensures there's only one of something, like your super-secret diary, in your program. 
+  It's handy when you want to manage and control access to a single instance of an object.
+
+
+  
+  ------------------------------------------------------------------------------------------------------------------------------------------------------------
+Eager vs Lazy loading in single design pattern  with code in c++
+  Eager loading and lazy loading are two different approaches for implementing the Singleton design pattern in C++. 
+  Let's explore both of them with code examples.
+
+**Eager Loading:**
+
+In eager loading, the singleton instance is created as soon as the program starts, regardless of whether it's immediately needed or not. 
+  Here's a C++ example:
+
+```cpp
+#include <iostream>
+
+class Singleton {
+private:
+    static Singleton* instance;
+
+    Singleton() {
+        std::cout << "Singleton instance created." << std::endl;
+    }
+
+public:
+    static Singleton* getInstance() {
+        return instance;
+    }
+};
+
+// Initialize the instance at the start of the program
+Singleton* Singleton::instance = new Singleton();
+
+int main() {
+    // The instance is created when the program starts
+    Singleton* obj1 = Singleton::getInstance();
+    Singleton* obj2 = Singleton::getInstance();
+
+    return 0;
+}
+```
+
+In this example, the Singleton instance is created right when the program begins, regardless of whether you use it immediately or not.
+
+**Lazy Loading:**
+
+In lazy loading, the singleton instance is created only when it's first needed. This can save memory and resources because it's created on-demand. Here's a C++ example:
+
+```cpp
+#include <iostream>
+
+class Singleton {
+private:
+    static Singleton* instance;
+
+    Singleton() {
+        std::cout << "Singleton instance created." << std::endl;
+    }
+
+public:
+    static Singleton* getInstance() {
+        if (instance == nullptr) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+};
+
+// Initialize the instance as nullptr
+Singleton* Singleton::instance = nullptr;
+
+int main() {
+    // The instance is created when you first call getInstance
+    Singleton* obj1 = Singleton::getInstance();
+    Singleton* obj2 = Singleton::getInstance();
+
+    return 0;
+}
+```
+
+In this example, the Singleton instance is only created when you call `getInstance` for the first time. If you don't call `getInstance`, 
+  it won't be created at all, saving resources until it's actually needed.
+
+**Eager Loading Pros and Cons:**
+- **Pros:** The instance is always ready for use; you don't need to worry about creating it when needed.
+- **Cons:** It consumes memory even if you never use the instance, which might not be efficient.
+
+**Lazy Loading Pros and Cons:**
+- **Pros:** It saves memory by creating the instance only when required.
+- **Cons:** It may introduce a small delay when you first access the instance, and it requires additional code to handle lazy initialization.
+
+You can choose between eager and lazy loading based on your specific requirements. Eager loading is simple but consumes memory upfront,
+while lazy loading is more efficient in terms of memory but involves additional logic for lazy initialization. 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+How to implement thread safety in above singleton design pattern in c++
+
+  To implement thread safety in a Singleton design pattern in C++, you need to ensure that the creation of the singleton instance 
+is synchronized so that multiple threads cannot simultaneously create separate instances. There are a few ways to achieve thread safety. 
+  Here's one common approach using a mutex:
+
+```cpp
+#include <iostream>
+#include <mutex>
+
+class Singleton {
+private:
+    static Singleton* instance;
+    static std::mutex mtx; // Mutex for synchronization
+
+    Singleton() {
+        std::cout << "Singleton instance created." << std::endl;
+    }
+
+public:
+    static Singleton* getInstance() {
+        if (instance == nullptr) {
+            std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+            if (instance == nullptr) {
+                instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+};
+
+// Initialize the instance as nullptr
+Singleton* Singleton::instance = nullptr;
+std::mutex Singleton::mtx; // Initialize the mutex
+
+int main() {
+    // The instance is created when you first call getInstance
+    Singleton* obj1 = Singleton::getInstance();
+    Singleton* obj2 = Singleton::getInstance();
+
+    return 0;
+}
+```
+
+In this code:
+
+1. We use a `std::mutex` (mutex stands for mutual exclusion) to ensure that only one thread at a time can create the singleton instance.
+
+2. Inside the `getInstance` method, we first check if the `instance` is `nullptr`. If it is, this means that no instance has been created yet.
+
+3. We then use `std::lock_guard` to lock the mutex. This ensures that if multiple threads try to access `getInstance` 
+simultaneously, only one of them can proceed while the others are blocked.
+
+4. Once a thread acquires the lock, it checks again if `instance` is still `nullptr`. If it is, that thread creates the instance, 
+and then other threads can continue.
+
+This way, we ensure that even in a multi-threaded environment, only one instance of the Singleton is created, and it's done safely 
+without causing race conditions.
+
+Remember that while this approach provides thread safety for Singleton instantiation, you should also consider thread safety for other methods 
+and data within the Singleton class if they are accessed concurrently by multiple threads.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
